a7e66437c3bcd8be4c515f03715e3846957d8aac
aimbot1526 <anshjaamwal2002@gmail.com>
2022-03-03 21:09:30 +0530

    added README.md

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//app.get('/', (req, res) => {
+//  res.send('Hello World!')
+//})
+
+app.get('/', function (req, res) {
+  child_process.exec(
+    'gzip ' + req.query.file_path,
+    function (err, data) {
+      console.log('err: ', err)
+      console.log('data: ', data);
+    });
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
